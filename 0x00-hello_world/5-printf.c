a1bd4e182df8bdee16008a58f266b4fc097ce533
Said0u <saidoufkire@gmail.com>
2023-05-18 18:06:36 -0700

    pushing a dir interesting

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Hello, printf
+ * Description: Using printf to print text
+ * Return: 0 to exit the programme process
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+It's gonna be a looong hard days but cool days
